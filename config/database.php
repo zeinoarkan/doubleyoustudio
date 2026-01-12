e982b0cb72020540316cef40cf93fb4c59694fad
zenn <zeinoarkananta@gmail.com>
2026-01-12 10:27:56 +0700

    hosting

--- config/database.php
+++ config/database.php
@@ -95,7 +95,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
